9fbd8d8b45b3b7997a4f06b9658c1936ef3dff5d
wlghek114 <wlghek0418@gmail.com>
2024-10-08 18:04:57 +0900

    241008

--- Airbusan/script.js
+++ Airbusan/script.js
@@ -25,3 +25,14 @@ addEventListener("load", () => {
   }
 });
 
+let i = 0;
+const guideLet = document.querySelectorAll('.guide-let-box > li')
+function slide(){
+  (i === guideLet.length) && (i = 0)
+  console.log(guideLet)
+  guideLet[i].className = 'topbt';
+  guideLet[(i+1) % guideLet.length].className = 'top'
+  guideLet[(i+2) % guideLet.length].className = 'topplus'
+  i++;
+}
+setInterval(slide, 5500);
