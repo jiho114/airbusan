6ae8c6fde289b0bc62af7d21c37ed7b09328f383
wlghek114 <wlghek0418@gmail.com>
2024-10-15 17:59:07 +0900

    241015

--- Airbusan/script.js
+++ Airbusan/script.js
@@ -36,3 +36,48 @@ function slide(){
   i++;
 }
 setInterval(slide, 5500);
+
+let mainTapSlide = document.querySelector('.slide-menu')
+let mainTapSlideLi = document.querySelectorAll('.slide-menu>li')
+let showNum = 4;
+let total = mainTapSlideLi.length;
+let liWidth = mainTapSlideLi[0].offsetWidth;
+
+for(let i = 0; i < showNum; i++){
+  const copyobj = mainTapSlideLi[i].cloneNode(true)
+  mainTapSlide.appendChild(copyobj)
+}
+
+let num = 0;
+document.querySelector('#next').addEventListener('click', ()=>{
+  if(num === total){
+    num = 0;
+    mainTapSlide.style.transition = 'none'
+    mainTapSlide.style.marginLeft = '0%'
+    console.log(num)
+  }
+  setTimeout(function(){
+    num++;
+    mainTapSlide.style.transition = 'margin-left .5s'
+    mainTapSlide.style.marginLeft = `${-liWidth*num}px`
+  },50)
+})
+
+document.querySelector('#prev').addEventListener('click', ()=>{
+  if(num === 0){
+    num = total;
+    mainTapSlide.style.transition ='none';
+    mainTapSlide.style.marginLeft = `${-liWidth*num}px`
+  }
+  setTimeout(function(){
+     num--;
+     mainTapSlide.style.transition ='margin-left .5s';
+     mainTapSlide.style.marginLeft = `${-liWidth*num}px`
+  },50)
+})
+
+let subslideBox = document.querySelector('.tapslide')
+let subslideLi = document.querySelectorAll('.tapslide>li')
+
+
+
