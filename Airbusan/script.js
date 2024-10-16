5ab019ac13af21695cd9e89d6ba93275e0afaf6e
wlghek114 <wlghek0418@gmail.com>
2024-10-16 17:55:47 +0900

    241016

--- Airbusan/script.js
+++ Airbusan/script.js
@@ -29,7 +29,6 @@ let i = 0;
 const guideLet = document.querySelectorAll('.guide-let-box > li')
 function slide(){
   (i === guideLet.length) && (i = 0)
-  console.log(guideLet)
   guideLet[i].className = 'topbt';
   guideLet[(i+1) % guideLet.length].className = 'top'
   guideLet[(i+2) % guideLet.length].className = 'topplus'
@@ -54,7 +53,6 @@ document.querySelector('#next').addEventListener('click', ()=>{
     num = 0;
     mainTapSlide.style.transition = 'none'
     mainTapSlide.style.marginLeft = '0%'
-    console.log(num)
   }
   setTimeout(function(){
     num++;
@@ -70,15 +68,60 @@ document.querySelector('#prev').addEventListener('click', ()=>{
     mainTapSlide.style.marginLeft = `${-liWidth*num}px`
   }
   setTimeout(function(){
-     num--;
-     mainTapSlide.style.transition ='margin-left .5s';
+    num--;
+    mainTapSlide.style.transition ='margin-left .5s';
      mainTapSlide.style.marginLeft = `${-liWidth*num}px`
-  },50)
+    },50)
+  })
+
+  
+  //하단 탭 슬라이드 배너
+  //선언
+  let subSlideBox = document.querySelector('.tapslide>ul')
+  let subSlideLi = document.querySelectorAll('.tapslide>ul>li')
+  // let subShow = 2;
+  // let subTotal = subSlideLi.length;
+  let subSlideWidth = subSlideLi[0].offsetWidth;
+  let bullets = document.querySelectorAll('.bullets>li')
+
+//슬라이드 복제
+  // for(let i = 0; i < subShow; i++){
+  //   const copyobj = subSlideLi[i].cloneNode(true)
+  //   subSlideBox.appendChild(copyobj)
+  // }
+
+// next 버튼 활성화
+let subNum = 0;
+document.querySelector('#subslidetap-right').addEventListener('click', ()=>{
+  subNum++;
+
+  if(subNum >= 4) subNum = 4;
+  
+  subSlideBox.style.transition = 'margin-left .5s'
+  subSlideBox.style.marginLeft = `${-subSlideWidth*subNum}px`;
+  
+  for(let i = 0; i < bullets.length; i++){
+    bullets[i].classList.remove('on')
+    // console.log(bullets[i])
+  } 
+  
+  bullets[subNum].classList.add('on')
 })
 
-let subslideBox = document.querySelector('.tapslide')
-let subslideLi = document.querySelectorAll('.tapslide>li')
+//prev버튼 활성화
+document.querySelector('#subslidetap-left').addEventListener('click', ()=>{
+  subNum--;
 
-
-
+  if(subNum <= 0) subNum = 0;
+  
+  subSlideBox.style.transition = 'margin-left .5s'
+  subSlideBox.style.marginLeft = `${-subSlideWidth*subNum}px`;
+  
+  for(let i = 0; i < bullets.length; i++){
+    bullets[i].classList.remove('on')
+    // console.log(bullets[i])
+  } 
+  
+  bullets[subNum].classList.add('on')
+})
 
